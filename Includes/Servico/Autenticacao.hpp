84d06a1d21cba77a835067b252668d11848e10fe
Debzzan <ddbaby2004@gmail.com>
2025-01-27 20:37:05 -0300

    ok

---
+++ Includes/Servico/Autenticacao.hpp
@@ -0,0 +1,15 @@
+#ifndef AUTENTICACAO_HPP_INCLUDED
+#define AUTENTICACAO_HPP_INCLUDED
+#include "../TP1-PARTE2/Includes/Entidades/conta.hpp"
+
+
+class Autenticacao : public Conta
+{
+
+
+
+
+};
+
+
+#endif
---
+++ Includes/Servico/Usuario.hpp
@@ -0,0 +1,23 @@
+#ifndef USUARIO_HPP_INCLUDED
+#define USUARIO_HPP_INCLUDED
+#include "../TP1-PARTE2/Includes/Entidades/conta.hpp"
+
+
+class Usuario : public Conta
+{
+private:
+ void CriarConta();
+ void ExcluirConta();
+ void Login();
+
+
+public:
+ 
+ 
+
+
+
+};
+
+
+#endif
